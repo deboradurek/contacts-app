c06c9a2d32aaa8f501eb785e8124169527036336
Debora Durek <debora.durek@gmail.com>
2021-04-03 19:54:00 -0500

    feat: Apply filter for contacts list

--- client/src/ListContacts.js
+++ client/src/ListContacts.js
@@ -11,6 +11,7 @@ class ListContacts extends Component {
     query: '',
   };
 
+  // New state for query will be the actual query in the input minus any spaces
   updateQuery = (query) => {
     this.setState(() => ({
       query: query.trim(),
@@ -18,6 +19,15 @@ class ListContacts extends Component {
   };
 
   render() {
+    const { contacts, onDeleteContact } = this.props;
+    const { query } = this.state;
+
+    // New array after checking if form state is still empty
+    const showingContacts =
+      query === ''
+        ? contacts
+        : contacts.filter((c) => c.name.toLowerCase().includes(query.toLowerCase()));
+
     return (
       <div className="list-contacts">
         <div className="list-contacts-top">
@@ -25,12 +35,12 @@ class ListContacts extends Component {
             className="search-contacts"
             type="text"
             placeholder="Search Contacts"
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
         <ol className="contact-list">
-          {this.props.contacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className="contact-list-item">
               <div
                 className="contact-avatar"
@@ -40,10 +50,7 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.handle}</p>
               </div>
-              <button
-                className="contact-remove"
-                onClick={() => this.props.onDeleteContact(contact)}
-              >
+              <button className="contact-remove" onClick={() => onDeleteContact(contact)}>
                 Remove
               </button>
             </li>
