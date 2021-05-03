d109b484b4abeafee1b35508409a4540d55bf000
Debora Durek <debora.durek@gmail.com>
2021-05-02 21:29:38 -0500

    feat: Remove hardcoded state and add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,47 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: 'karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg',
-      },
-      {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: 'richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg',
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: 'tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg',
-      },
-    ],
+    contacts: [],
   };
 
-  // Create a method to remove a contact from the list
+
+  };
+
+  // Remove a contact from the list
   removeContact = (contact) => {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter((c) => {
         return c.id !== contact.id;
       }),
     }));
+
   };
 
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+          )}
+        />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact);
+                history.push('/');
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
