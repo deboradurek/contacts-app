6844615ccb0d4d675c45deefe4b93fe141c2b367
Debora Durek <debora.durek@gmail.com>
2021-05-02 21:28:11 -0500

    feat: Implement onSubmit method

--- client/src/CreateContact.js
+++ client/src/CreateContact.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const values = serializeForm(event.target, { hash: true });
+
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values);
+    }
+  };
+
   render() {
     return (
       <div>
         <Link className="close-create-contact" to="/">
           Close
         </Link>
-        <form className="create-contact-form">
+        <form className="create-contact-form" onSubmit={this.handleSubmit}>
           <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64} />
           <div className="create-contact-details">
             <input type="text" name="name" placeholder="Name" />
