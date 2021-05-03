5c8d81702aa901d7712c59709d6b9451111f7262
Debora Durek <debora.durek@gmail.com>
2021-05-02 21:27:31 -0500

    feat: Add component to show avatar

--- client/src/CreateContact.js
+++ client/src/CreateContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   render() {
@@ -9,6 +10,7 @@ class CreateContact extends Component {
           Close
         </Link>
         <form className="create-contact-form">
+          <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64} />
           <div className="create-contact-details">
             <input type="text" name="name" placeholder="Name" />
             <input type="text" name="handle" placeholder="Handle" />
